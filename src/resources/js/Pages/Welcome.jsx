11ab281dbf1326924015eb240aedd7b1d316abea
sho-honma <honma.engineer@gmail.com>
2023-02-10 04:26:51 +0900

    🎨 Homeコンポーネント用にコードを修正

--- src/resources/js/Pages/Welcome.jsx
+++ src/resources/js/Pages/Welcome.jsx
@@ -1,23 +1,31 @@
-import { Link, Head } from '@inertiajs/react';
+import { Link, Head } from "@inertiajs/react";
+import { Home } from "../Components/Home/Home";
 
 export default function Welcome(props) {
     return (
         <>
             <Head title="Welcome" />
-            <div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+            <Home />
+            <div className="relative flex items-top justify-center  bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
                 <div className="fixed top-0 right-0 px-6 py-4 sm:block">
                     {props.auth.user ? (
-                        <Link href={route('dashboard')} className="text-sm text-gray-700 dark:text-gray-500 underline">
+                        <Link
+                            href={route("dashboard")}
+                            className="text-sm text-gray-700 dark:text-gray-500 underline"
+                        >
                             Dashboard
                         </Link>
                     ) : (
                         <>
-                            <Link href={route('login')} className="text-sm text-gray-700 dark:text-gray-500 underline">
+                            <Link
+                                href={route("login")}
+                                className="text-sm text-gray-700 dark:text-gray-500 underline"
+                            >
                                 Log in
                             </Link>
 
                             <Link
-                                href={route('register')}
+                                href={route("register")}
                                 className="ml-4 text-sm text-gray-700 dark:text-gray-500 underline"
                             >
                                 Register
@@ -26,7 +34,7 @@ export default function Welcome(props) {
                     )}
                 </div>
 
-                <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
+                {/* <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
                     <div className="flex justify-center pt-8 sm:justify-start sm:pt-0">
                         <svg
                             viewBox="0 0 651 192"
@@ -252,7 +260,7 @@ export default function Welcome(props) {
                             Laravel v{props.laravelVersion} (PHP v{props.phpVersion})
                         </div>
                     </div>
-                </div>
+                </div> */}
             </div>
         </>
     );

be285c4476882c2108b90a8084cdedc5304a9ce4
sho-honma <honma.engineer@gmail.com>
2023-02-12 16:14:55 +0900

    🍱 マイグレーションの設定

---
+++ src/database/migrations/2023_02_12_155806_create_details_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('details', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('work_id');
+            $table->integer('section_id');
+            $table->string('detail_title');
+            $table->text('description');
+            $table->integer('important');
+            $table->integer('date');
+            $table->integer('color');
+            $table->timestamps('published');
+            $table->foreign('work_id')->references('id')->on('works');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('details');
+    }
+};
